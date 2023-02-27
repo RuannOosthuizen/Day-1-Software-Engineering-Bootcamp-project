e5f399cc1ef555a41c4fe65d41197f27930cdd63
Ruann <pr0h3ctic@gmail.com>
2023-02-27 21:18:53 +0100

    first commit

---
+++ hallo_world.py
@@ -0,0 +1,12 @@
+# Here I am creating the variable that will store the users input which will be their name.
+user_name = input("Please type in your name: ")
+# Here I am printing the users name by using the print funtion the print the varibale containing the data which is their name. 
+print(user_name)
+
+# Here I am doing a simailer code to before asking for the users name and storing it in a variable.
+age = input("Please enter your age: ")
+# Here I am printing the users age and name like before using the print funtion and the varbiale with some extra text.
+print("Hallo " + user_name + " Your age is " + age)
+
+# Here I am simply prinitng the word Hello World on a new line using the print funtion.
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,12 @@
+# Here I am using the funtion input to gather the data from the user for their 3 favourite foods.
+item1 = input("Please wrtie down your food you want to order: ")
+item2 = input("Please write down the drink you wish to order: ")
+item3 = input("Please write down the desert you wish to order: ")
+
+# Here i am simply printing a string a text to be displayed before the uses input is prinitng.
+print("Confirming your order! You have ordered:")
+
+#Here I am recalling the previouse input from the user via variables and printing them on new lines using the print funtion.
+print(item1)
+print(item2)
+print(item3)
